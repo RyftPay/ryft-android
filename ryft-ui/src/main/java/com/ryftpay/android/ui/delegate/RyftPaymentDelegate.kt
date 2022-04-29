8fc2c0aa9721be95ccc01e2fd2630b753134dafe
Mike Ridge <81687680+mike-ridge@users.noreply.github.com>
2022-04-29 18:43:52 +0100

    [R1281] Google pay ready check (#5)
    * [R1281] Google pay ready check
    
    - Add google pay wallet dependency and implement isReadyToPay request, powering whether or not we show google pay on the payment fragment
    
    * Make googlePayName nullable for 'Unknown' card type and ensure no null names are returned from getGooglePaySupportedTypeNames

--- ryft-ui/src/main/java/com/ryftpay/android/ui/delegate/RyftPaymentDelegate.kt
+++ ryft-ui/src/main/java/com/ryftpay/android/ui/delegate/RyftPaymentDelegate.kt
@@ -3,5 +3,5 @@ package com.ryftpay.android.ui.delegate
 import android.view.View
 
 internal interface RyftPaymentDelegate {
-    fun onViewCreated(root: View)
+    fun onViewCreated(root: View, showGooglePay: Boolean)
 }
