dcbc745015af80e00fbd68164a2f07f0712edff3
Mike Ridge <mike@getbutlr.com>
2022-06-30 15:20:02 +0100

    [Tech] Bump version
    - Bump version to 1.1.0 ready for release

--- ryft-core/src/test/java/com/ryftpay/android/core/model/RyftTest.kt
+++ ryft-core/src/test/java/com/ryftpay/android/core/model/RyftTest.kt
@@ -7,11 +7,11 @@ internal class RyftTest {
 
     @Test
     fun `Version should have initial version of sdk`() {
-        Ryft.VERSION shouldBeEqualTo "1.0.0"
+        Ryft.VERSION shouldBeEqualTo "1.1.0"
     }
 
     @Test
     fun `User agent should identify sdk with initial version`() {
-        Ryft.USER_AGENT shouldBeEqualTo "ryft-sdk-android/1.0.0"
+        Ryft.USER_AGENT shouldBeEqualTo "ryft-sdk-android/1.1.0"
     }
 }
