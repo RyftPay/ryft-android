0abaa178d9c5000860ebe7c53951ec83dddf6a68
Mike Ridge <mike@getbutlr.com>
2022-11-07 10:44:55 +0000

    [Tech] Bump version
    - Bump version to 1.4.0

--- ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
+++ ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
@@ -1,6 +1,6 @@
 package com.ryftpay.android.core.model
 
 internal object Ryft {
-    internal const val VERSION = "1.3.0"
+    internal const val VERSION = "1.4.0"
     internal const val USER_AGENT = "ryft-sdk-android/$VERSION"
 }
