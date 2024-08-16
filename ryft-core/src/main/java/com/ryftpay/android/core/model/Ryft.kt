025402cdb6ba8cfd4bb4c69c8879351ece456189
Mike Ridge <81687680+mike-ridge@users.noreply.github.com>
2024-08-16 11:03:07 +0100

    [Tech] Bump version (#46)
    - Bump version to 1.6.1

--- ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
+++ ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
@@ -1,6 +1,6 @@
 package com.ryftpay.android.core.model
 
 internal object Ryft {
-    internal const val VERSION = "1.6.0"
+    internal const val VERSION = "1.6.1"
     internal const val USER_AGENT = "ryft-sdk-android/$VERSION"
 }
